66e17ebc0230e810f3fffa322391f71054e91282
taylorwoodman <taylor.woodman711@gmail.com>
2019-06-04 20:11:01 -0600

    Close Enough!

--- react-iii-afternoon/src/App.js
+++ react-iii-afternoon/src/App.js
@@ -1,58 +1,82 @@
-import React from 'react';
-import './App.css';
-import data from './data.js';
+import React from "react";
+import "./App.css";
+import data from "./data.js";
 
-function Item(props){
+function Item(props) {
   return (
-    <div className="info">
-      <div>Page {props.displayUser.id}/25</div>
-      <div>{props.displayUser.name.first} {props.displayUser.name.last}</div>
-      <div>From: {props.displayUser.city}, {props.displayUser.country}</div>
-      <div>Job Title: {props.displayUser.title}</div>
-      <div>Employer: {props.displayUser.employer}</div>
-      <div>Favorite Movies: <ol> <li>{props.displayUser.favoriteMovies[0]}</li>
-      <li>{props.displayUser.favoriteMovies[1]}</li>
-      <li>{props.displayUser.favoriteMovies[2]}</li>
-      </ol>
+    <div className="Box">
+      <div className="Page">{props.displayUser.id}/25</div>
+      <div className="Name">
+        {props.displayUser.name.first} {props.displayUser.name.last}
       </div>
-      
+      <p>
+        <div>
+          <b>From:</b> {props.displayUser.city}, {props.displayUser.country}
+        </div>
+        <div>
+          <b>Job Title:</b> {props.displayUser.title}
+        </div>
+        <div>
+          <b>Employer:</b> {props.displayUser.employer}
+        </div>
+      </p>
+      <div className="Movies">
+        <b>Favorite Movies:</b>{" "}
+        <ol>
+          <li>{props.displayUser.favoriteMovies[0]}</li>
+          <li>{props.displayUser.favoriteMovies[1]}</li>
+          <li>{props.displayUser.favoriteMovies[2]}</li>
+        </ol>
+      </div>
     </div>
-  )
- 
-
+  );
 }
 
-class App extends React.Component{
-  constructor(){
+class App extends React.Component {
+  constructor() {
     super();
 
     this.state = {
-    page: 1,
-    user: data[0]
-    }
-
+      page: 0,
+      user: data[0]
+    };
+    this.nextUser = this.nextUser.bind(this);
+    this.perviousUser = this.perviousUser.bind(this);
   }
 
+  nextUser() {
+    let { page } = this.state;
+    page = page + 1;
+    this.setState({ page: page, user: data[page] });
+  }
 
-render() {
+  perviousUser() {
+    let { page } = this.state;
+    page = page - 1;
+    this.setState({ page: page, user: data[page] });
+  }
 
-
-  return (
-    <div className="App">
-      <div className="Header"></div>
-      <div>
-        <div className="InfoBox">
-      <Item displayUser={this.state.user}/>
+  render() {
+    return (
+      <div className="App">
+        <div className="Header">
+          <div className="Home">Home</div>
         </div>
-        <div className="Buttons">
-      <button className='Previous' onClick={this.handleClick}>{'<'} Previous</button>
-      <button className='Next'>Next {'>'}</button>
+        <div>
+          <div className="InfoBox">
+            <Item displayUser={this.state.user} />
+          </div>
+          <div className="Buttons">
+            <button className="Previous" onClick={this.perviousUser}>
+              {"<"} Previous
+            </button>
+            <button className="Next" onClick={this.nextUser}>
+              Next {">"}
+            </button>
+          </div>
+        </div>
       </div>
-      </div>
-      </div>
-      
-      
-  );
-}
+    );
+  }
 }
 export default App;
